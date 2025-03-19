f741b400f23d4c06e01e8c303cfc4fd7a5286197
aymanboujjar <aymenboujjar12@gmail.com>
2025-03-19 12:50:01 +0000

    change

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -4,7 +4,7 @@ export default function AppLogo() {
     return (
         <>
            
-            <img className='w-10' src="/storage/logo.svg" alt="" />
+            <img className='w-10' src="/assets/images/logo.svg" alt="" />
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">Yes Learning</span>
             </div>
