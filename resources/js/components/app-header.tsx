30d2c9343398f29e225646cb8ce971fe4cceb601
forkanimahdi <forkanimahdi@gmail.com>
2025-03-19 14:19:57 +0000

    switch ngo to navbar and  admin to sidebar

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -11,7 +11,7 @@ import { useInitials } from '@/hooks/use-initials';
 import { cn } from '@/lib/utils';
 import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
+import { Award, BookOpen, Folder, LayoutGrid, LibraryBig, Menu, Search } from 'lucide-react';
 import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
 
@@ -20,6 +20,21 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         url: '/dashboard',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Courses',
+        url: '/course',
+        icon: BookOpen,
+    },
+    {
+        title: 'Achivement',
+        url: '/achivement',
+        icon: Award,
+    },
+    {
+        title: 'Library',
+        url: '/library',
+        icon: LibraryBig,
     },
 ];
 
--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,14 +1,22 @@
-import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
-import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { usePage } from "@inertiajs/react";
+import { type BreadcrumbItem } from "@/types";
+import { type ReactNode } from "react";
+import AppHeaderLayout from "@/layouts/app/app-header-layout";
+import AppSidebarLayout from "@/layouts/app/app-sidebar-layout";
 
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
-    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
-    </AppLayoutTemplate>
-);
+export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => {
+    const { auth } = usePage<{ auth: { user: { role: string } } }>().props;
+
+    const LayoutComponent = auth?.user?.role === "admin" ? AppSidebarLayout : AppHeaderLayout;
+
+    return (
+        <LayoutComponent breadcrumbs={breadcrumbs} {...props}>
+            {children}
+        </LayoutComponent>
+    );
+};
