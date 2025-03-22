ca11d0c3d6665643b72446baa71eba488a5bef3c
sarachafikidrissi <chafikidrissisara@gmail.com>
2025-03-22 18:15:35 +0000

    recieve data from yes bacend and add it to user table

---
+++ app/Mail/CredentialsMailer.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class CredentialsMailer extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $randomPassword;
+    public function __construct($randomPassword)
+    {
+        $this->randomPassword = $randomPassword;
+        Log::info('hiiii');
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Credentials Mailer',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.credentialsMail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\ReceiveDataController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// * the recieved data coming from yes backend
+Route::post('/receive-data', [ReceiveDataController::class, 'store']);
+// Route::get('/get-data', [ReceiveDataController::class, 'index']);
