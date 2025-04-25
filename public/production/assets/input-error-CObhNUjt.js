3072f0ab7fbb4371cef89187bf2f7bd5ea03c09d
forkanimahdi <forkanimahdi@gmail.com>
2025-04-25 13:23:54 +0100

    auto deploy

---
+++ public/production/assets/input-error-CObhNUjt.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Cray-2Qw.js";import{a as o}from"./button-Cj0jXLlE.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
