e10cf2071dbefba8548139f7b8a63982e1f30fda
sarachafikidrissi <chafikidrissisara@gmail.com>
2025-04-25 11:19:16 +0100

    redirect user and admin to their dashboard when they ae to trying to access unauthorized routes

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', "role:user"])->group(function () {
