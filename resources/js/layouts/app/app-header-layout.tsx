fcca8ad69b95f5da2bec686c715001d1fe4d3be1
Ayman Boujjar <aymenboujjar12@gmail.com>
2025-05-05 16:46:05 +0100

    modification in the ngo footer

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -39,41 +39,14 @@ export default function AppHeaderLayout({ children, breadcrumbs }: PropsWithChil
                         </div>
 
                         <div className="flex gap-4">
-                            <a href="#" className="text-gray-600 hover:text-gray-900">
-                                <span className="sr-only">Twitter</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="h-5 w-5"
-                                >
-                                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
+
+                            <a target='_blank' href="https://www.linkedin.com/company/yes-summit-africa/" className="text-gray-600 hover:text-gray-900">
+                                <span className="sr-only">linkedin</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
                                 </svg>
                             </a>
-                            <a href="#" className="text-gray-600 hover:text-gray-900">
-                                <span className="sr-only">Facebook</span>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="h-5 w-5"
-                                >
-                                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                                </svg>
-                            </a>
-                            <a href="#" className="text-gray-600 hover:text-gray-900">
+                            <a target='_blank' href="https://www.instagram.com/yes_summit_africa?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="text-gray-600 hover:text-gray-900">
                                 <span className="sr-only">Instagram</span>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -117,13 +90,13 @@ export default function AppHeaderLayout({ children, breadcrumbs }: PropsWithChil
                                 ar="شركاؤنا"
                             />
                         </h2>
-                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 justify-items-center items-center px-4">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-11 gap-4 justify-items-center items-center px-4">
                             {sponsors.map((filename, index) => (
                                 <img
                                     key={index}
                                     src={`/assets/images/sponsors/${filename}`}
                                     alt={`Sponsor ${index + 1}`}
-                                    className="h-12 w-auto object-contain  hover:grayscale-0 transition"
+                                    className="w-15  object-contain  hover:grayscale-0 transition"
                                 />
                             ))}
                         </div>
