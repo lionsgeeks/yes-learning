27d7d892b571e63377c4c5a112264164308639d4
osama-jeb <oussamajebrane98@gmail.com>
2025-05-29 12:20:47 +0100

    user table to cards[1;5D[1;5D[1;5D[D[D[D[D[D[D[D[D[D[D[D[Ds[C[C[D[D table to cards

--- resources/js/pages/ngos/index.jsx
+++ resources/js/pages/ngos/index.jsx
@@ -5,16 +5,20 @@ import { Head, router, useForm, usePage } from '@inertiajs/react';
 import axios from 'axios';
 import { useState } from 'react';
 import { toast } from 'sonner';
+import { UserInfo } from '@/components/user-info';
+import { Eye } from 'lucide-react';
+
 
 
 const breadcrumbs = [
     {
-        title: 'NGOs',
-        href: '/ngos',
+        title: 'Users',
+        href: '/users',
     },
 ];
 
 const NGOsTable = () => {
+
     const { ngos } = usePage().props;
     const [searchQuery, setSearchQuery] = useState('');
     const { post, data, setData } = useForm();
@@ -65,10 +69,10 @@ const NGOsTable = () => {
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Ngo's" />
+            <Head title="Users" />
             <div className="p-6">
                 <div className="mb-4 flex items-center justify-between">
-                    <h1 className="mb-4 text-xl font-bold">NGOs List</h1>
+                    <h1 className="mb-4 text-xl font-bold">Users List</h1>
                     <input
                         type="text"
                         placeholder="Search by name"
@@ -77,8 +81,8 @@ const NGOsTable = () => {
                         className="w- rounded-md border border-gray-300 p-2"
                     />
                 </div>
-                <div className="overflow-x-auto">
-                    <Table className="min-w-full">
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+                    {/* <Table className="min-w-full">
                         <TableHeader>
                             <TableRow>
                                 <TableHead>Name</TableHead>
@@ -120,7 +124,49 @@ const NGOsTable = () => {
                                 </TableRow>
                             ))}
                         </TableBody>
-                    </Table>
+                    </Table> */}
+                    {
+                        filteredNgos.map((ngo, index) => (
+                            <div key={index} className='p-2 relative flex flex-col items-center gap-y-5 shadow space-y-2 border-l-2 border-alpha rounded'
+
+                            >
+                                <button
+                                    className='text-alpha/80 cursor-pointer absolute top-[3%] right-[3%]'
+                                    onClick={() => { router.visit('ngos/' + ngo.id) }}
+                                >
+                                    <Eye />
+                                </button>
+                                <UserInfo user={ngo} />
+                                {/* <p><span className='font-semibold text-alpha'>Name:</span> {ngo.name}</p> */}
+                                {/* <p><span className='font-semibold text-alpha'>Email:</span> {ngo.email}</p> */}
+                                {/* <p><span className='font-semibold text-alpha'>Selected Language:</span> {ngo.language == 'en' ? 'English' : ngo.language == "ar" ? 'Arabic' : 'French'}</p> */}
+                                {/* <p><span className='font-semibold text-alpha'>Sign-up Date:</span> {new Date(ngo.created_at).toLocaleString()}</p> */}
+
+                                {ngo.invitedToApp ? (
+                                    <Button className="bg-green-500" disabled>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-5 w-5"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                            aria-hidden="true"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 00-2 0v4.586l-2.293-2.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l4-4a1 1 0 00-1.414-1.414L11 9.586V5z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        Invited
+                                    </Button>
+                                ) : (
+                                    <Button onClick={() => handleInvite(ngo)}>Invite to App</Button>
+                                )}
+
+
+                            </div>
+                        ))
+                    }
                 </div>
                 <div className="mt-4 flex w-full items-center justify-between rounded-md bg-gray-100 p-4">
                     <Button disabled={!ngos.prev_page_url} onClick={() => goToPage(ngos.current_page - 1)}>
