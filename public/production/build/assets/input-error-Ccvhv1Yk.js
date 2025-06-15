278c8d757aabbc486a3f4c6f40bf67cbc4e2b942
forkanimahdi <forkanimahdi@gmail.com>
2025-06-15 13:07:20 +0100

    remove user seeder

--- public/production/build/assets/input-error-Ccvhv1Yk.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BO2Erc1k.js";import{a as o}from"./button-Bo_LbEuX.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
